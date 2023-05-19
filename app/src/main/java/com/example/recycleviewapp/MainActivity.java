82db0ba6d002977b0451faaf04bd781e9457b159
flutterdeveloper7070 <flutterdeveloper7070@gmail.com>
2023-05-19 14:38:34 +0530

    1

---
+++ app/src/main/java/com/example/recycleviewapp/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.recycleviewapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ArrayList<contact> arrcontact = new ArrayList<contact>();
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RecyclerView recyclerView = findViewById(R.id.Recyclerview);
+
+
+    }
+}
---
+++ app/src/main/java/com/example/recycleviewapp/contact.java
@@ -0,0 +1,15 @@
+package com.example.recycleviewapp;
+
+public class contact {
+
+    int image;
+    String name, number;
+
+    public contact(int image, String name, String number) {
+
+        this.image = image;
+        this.name = name;
+        this.number = number;
+
+    }
+}
